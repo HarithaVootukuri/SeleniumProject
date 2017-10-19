84c696ffbeff5c8e124a6a3d4bf87a40a6e6dfe6
haritha V <haritha513.skit@gmail.com>
2017-10-19 18:41:57 +0530

    hello

---
+++ src/SeleniumLearning/BrowserInvocation.java
@@ -0,0 +1,12 @@
+package SeleniumLearning;
+
+public class BrowserInvocation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Webdriver wd=new FirefoxDriver();
+		
+	}
+
+}
